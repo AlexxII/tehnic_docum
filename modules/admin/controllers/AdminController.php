6159ca6c3684168889cb24f5f208b1d6f53f2fae
AlexxII <murmus51@yandex.ru>
2018-11-12 11:48:29 +0300

    Текущие поправки - рабочая подверсия

--- modules/admin/controllers/AdminController.php
+++ modules/admin/controllers/AdminController.php
@@ -8,24 +8,41 @@ use app\modules\admin\models\Category;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
 
 /**
  * CategoryController implements the CRUD actions for Category model.
  */
 class AdminController extends Controller
 {
-  public function actionIndex()
-  {
-    return $this->render('index');
-  }
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['superAdmin'],      // доступ только с ролью superAdmin
+                    ],
+                ],
+            ],
+        ];
+    }
 
-  public function actionPlacement()
-  {
-    return $this->render('places');
-  }
 
-  public function actionTests()
-  {
-    return $this->render('tests');
-  }
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+    public function actionPlacement()
+    {
+        return $this->render('places');
+    }
+
+    public function actionTests()
+    {
+        return $this->render('tests');
+    }
 }
