bd4f6e657453abea0f07f68afee5e86c84b2b9b0
AlexxII <murmus51@yandex.ru>
2018-11-13 14:54:45 +0300

    Текущие поправки - рабочая подверсия

--- modules/admin/controllers/CategoryController.php
+++ modules/admin/controllers/CategoryController.php
@@ -10,125 +10,128 @@ use yii\web\Controller;
 class CategoryController extends Controller
 {
 
-  public function actionIndex()
-  {
-    return $this->render('index');
-  }
-
-  public function actionCategories()
-  {
-    $id = Category::find()->select('id, rgt, lft, root')->where(['=', 'lvl', 0])->all();
-    if (!$id) {
-      $data = array();
-      $data = [['title' => 'База данных пуста', 'key' => -999]];
-      return json_encode($data);
+    public function actionIndex()
+    {
+        return $this->render('index');
     }
-    $roots = Category::findOne($id)->tree();
-    return json_encode($roots);
-  }
 
-  public function actionMove ($item,$action,$second)
-  {
-    $item_model = Category::findOne($item);
-    $second_model = Category::findOne($second);
-    switch ($action){
-      case 'after':
-        $item_model->insertAfter($second_model);
-        break;
-      case 'before':
-        $item_model->insertBefore($second_model);
-        break;
-      case 'over':
-        $item_model->appendTo($second_model);
-        break;
+    public function actionCategories()
+    {
+        $id = Category::find()->select('id, rgt, lft, root')->where(['=', 'lvl', 0])->all();
+        if (!$id) {
+            $data = array();
+            $data = [['title' => 'База данных пуста', 'key' => -999]];
+            return json_encode($data);
+        }
+        $roots = Category::findOne($id)->tree();
+        return json_encode($roots);
     }
-    $parent = Category::findOne($second);
-    if (!$parent->children()->one()){
-      $parent->disabled = 0;
-      $parent->save();
-    } else {
-      $parent->disabled = 1;
-      $parent->save();
+
+    public function actionMove($item, $action, $second)
+    {
+        $item_model = Category::findOne($item);
+        $second_model = Category::findOne($second);
+        switch ($action) {
+            case 'after':
+                $item_model->insertAfter($second_model);
+                break;
+            case 'before':
+                $item_model->insertBefore($second_model);
+                break;
+            case 'over':
+                $item_model->appendTo($second_model);
+                break;
+        }
+        $parent = Category::findOne($second);
+        if (!$parent->children()->one()) {
+            $parent->disabled = 0;
+            $parent->save();
+        } else {
+            $parent->disabled = 1;
+            $parent->save();
+        }
+        if ($item_model->save()) {
+            return true;
+        }
+        return false;
     }
-    if ($item_model->save()) {
-      return true;
+
+    public function actionCreate($parentTitle, $title)
+    {
+        $data = [];
+        $category = Category::findOne(['name' => $parentTitle]);
+        $newSubcat = new Category(['name' => $title]);
+        $newSubcat->root = $category->id;
+        $newSubcat->appendTo($category);
+        if ($parent = $newSubcat->parents(1)->one()) {
+            $parent->disabled = 1;
+            $parent->save();
+        }
+        $data['acceptedTitle'] = $title;
+        return json_encode($data);
     }
-    return false;
-  }
 
-  public function actionCreate($parentTitle, $title)
-  {
-    $data = [];
-    $category = Category::findOne(['name' => $parentTitle]);
-    $newSubcat = new Category(['name' => $title]);
-    $newSubcat->root = $category->id;
-    $newSubcat->appendTo($category);
-    if ($parent = $newSubcat->parents(1)->one()){
-      $parent->disabled = 1;
-      $parent->save();
+    public function actionCreateRoot($title)
+    {
+        $newRoot = new Category(['name' => $title]);
+        $newRoot->makeRoot();
+        $data['acceptedTitle'] = $title;
+        return json_encode($data);
     }
-    $data['acceptedTitle'] = $title;
-    return json_encode($data);
-  }
 
-  public function actionCreateRoot($title)
-  {
-    $newRoot = new Category(['name' => $title]);
-    $newRoot->makeRoot();
-    $data['acceptedTitle'] = $title;
-    return json_encode($data);
-  }
-
-  public function actionUpdate($id, $title)
-  {
-    $category = Category::findOne(['id' => $id]);
-    $category->name = $title;
-    $category->save();
-    return true;
-  }
-
-  public function actionDelete($id)
-  {
-    // TODO: удаление или невидимый !!!!!!!
-    $category = Category::findOne(['id' => $id]);
-    $parent = $category->parents(1)->one();
-    $category->delete();
-    if (!$parent->children()->one()){
-      $parent->disabled = 0;
-      $parent->save();
-    } else {
-      $parent->disabled = 1;
-      $parent->save();
+    public function actionUpdate($id, $title)
+    {
+        $category = Category::findOne(['id' => $id]);
+        $category->name = $title;
+        $category->save();
+        return true;
     }
-  }
 
-  public function actionDeleteRoot($id)
-  {
-    $root = Category::findOne(['id' => $id]);
-    $root->deleteWithChildren();
-  }
+    public function actionDelete($id)
+    {
+        // TODO: удаление или невидимый !!!!!!!
+        $category = Category::findOne(['id' => $id]);
+        $parent = $category->parents(1)->one();
+        $category->delete();
+        if (!$parent->children()->one()) {
+            $parent->disabled = 0;
+            $parent->save();
+        } else {
+            $parent->disabled = 1;
+            $parent->save();
+        }
+    }
 
-  public function actionTests()
-  {
-    $array = array();
-    $leaves = Category::find()->select('id')->leaves()->orderBy('lft')->asArray()->all();
-    $categories = Category::find()->select('id')->where(['!=', 'lvl', '0'])->orderBy('lft')->asArray()->all();
-    $array['leaves'] = $leaves;
-    $array['cat'] = $categories;
-    return $this->render('tests', [
-        'model' => json_encode($array)
-    ]);
-  }
+    public function actionDeleteRoot()
+    {
+        if (!empty($_POST)) {
+            $id = $_POST['id'];
+            $root = Category::findOne(['id' => $id]);
+        }
+        $root->deleteWithChildren();
+    }
+
+    public function actionTests()
+    {
+        $array = array();
+        $leaves = Category::find()->select('id')->leaves()->orderBy('lft')->asArray()->all();
+        $categories = Category::find()->select('id')->where(['!=', 'lvl', '0'])->orderBy('lft')->asArray()->all();
+        $array['leaves'] = $leaves;
+        $array['cat'] = $categories;
+        return $this->render('tests', [
+            'model' => json_encode($array)
+        ]);
+    }
 
 
-  public function actionGetLeaves()
-  {
-    $array = array();
-    $leaves = Category::find()->select('id')->leaves()->orderBy('lft')->asArray()->all();
-    $categories = Category::find()->select('id')->where(['!=', 'lvl', '0'])->orderBy('lft')->asArray()->all();
-    $array['leaves'] = $leaves;
-    $array['cat'] = $categories;
-    return json_encode($array);
-  }
+    public function actionGetLeaves()
+    {
+        $array = array();
+        $leaves = Category::find()->select('id')->leaves()->orderBy('lft')->asArray()->all();
+        $categories = Category::find()->select('id')->where(['!=', 'lvl', '0'])->orderBy('lft')->asArray()->all();
+        $array['leaves'] = $leaves;
+        $array['cat'] = $categories;
+        return json_encode($array);
+    }
 
 }
